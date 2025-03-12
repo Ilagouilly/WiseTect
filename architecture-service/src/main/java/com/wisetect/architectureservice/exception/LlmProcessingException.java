2e3de79e5b26ad9dcb0e912c43b43492bdf5de8c
Ismail Lagouilly <i.lagouilly@gmail.com>
2025-03-12 20:35:18 +0100

    Add LlmProcessingException for improved error handling in LlmService 🚀🪄

---
+++ architecture-service/src/main/java/com/wisetect/architectureservice/exception/LlmProcessingException.java
@@ -0,0 +1,11 @@
+package com.wisetect.architectureservice.exception;
+
+public class LlmProcessingException extends RuntimeException {
+    public LlmProcessingException(String message) {
+        super(message);
+    }
+
+    public LlmProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
